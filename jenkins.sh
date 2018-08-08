8512a76a24f0d0eb60817f4d13dc75c951c0cb9f
aleksey suraykin <alexsuraykin@gmail.com>
2018-08-08 19:02:24 +0300

    update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -9,10 +9,17 @@ sudo docker-compose $COMPOSE_ARGS up -d
 # Выполнение модульного тестирования
 sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock > ERR=$?
 # Выполнение тестирования системы в целом, если модульное тестирование завершилось успешно
-if [ $ERR -eq 0 ]; then
+if [ $ERR=0 ]; then
   IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
   CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
-  if [ $CODE -ne 200 ]; then
+  if [ $CODE=200 ]; then
+    HASH=$(git rev-parse --short HEAD)
+    sudo docker tag jenkins_identidock dockerhost1:5000/identidock:$HASH
+    sudo docker tag jenkins_identidock dockerhost1:5000/identidock:newest
+    echo "Pushing"
+    sudo docker push dockerhost1:5000/identidock:$HASH
+    sudo docker push dockerhost1:5000/identidock:newest
+  else  
     echo "Site returned " $CODE
     ERR=1
   fi
